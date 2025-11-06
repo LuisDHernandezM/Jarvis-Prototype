ccd7b5c02b8076c6ef536a8847f112add4572ce7
Luis D. Hernandez <hernandezmorenoluisdaniel@gmail.com>
2025-11-06 11:04:38 -0800

    Importing ElevenLabs

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import os
+
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
+
+load_dotenv()
+
+AGENT_ID = os.getenv("AGENT_ID")
+API_KEY = os.getenv("API_KEY")
+
+user_name = "Luis"
+schedule = "Computer Science classes at 12:00; Gym with Maya at 17:00"
+prompt = f"You are a helpful assistant. Your interlocutor has the following schedule: {schedule}."
+first_message = f"Hello {user_name}, how can I help you today?"
